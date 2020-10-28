c9d431cb5346d0186e44d4190fef6412160c1431
debjit31 <debjitchatterjee31@gmail.com>
2020-10-28 23:34:28 +0530

    Last Commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.String(20))
+    content = db.Column(db.Text)
+    def __init__(self, title, subtitle, author, date_posted, content):
+        self.title=title
+        self.subtitle=subtitle
+        self.author=author
+        self.date_posted=date_posted.strftime('%B %d %Y')
+        self.content=content
+
+## home page
+@app.route("/")
+def index():
+    posts = Blogpost.query.order_by(Blogpost.id.desc()).all()
+    return render_template("index.html", posts=posts)
+
+## create your own post
+@app.route("/addPost")
+def addPost():
+    return render_template("add_post.html")
+
+
+@app.route('/postdata', methods = ["POST"])
+def postdata():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title,subtitle,author,datetime.now(),content)
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Blogpost.query.filter_by(id = post_id).one()
+    return render_template("post.html", post = post)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
